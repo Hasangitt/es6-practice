cd2d7e44ddbecd10e56bf0b8b5b970082a753a03
Hasangitt <pingpong038759@gmail.com>
2024-09-23 20:04:34 +0600

    es6

--- spread.js
+++ spread.js
@@ -1,3 +1,5 @@
+// get max  various types
+
 const math = Math.max(50,56,47,44,59,56,220);
 // console.log(math);
 
@@ -10,4 +12,18 @@ const arrayMax = Math.max(...numbers);
 
 const numbers2 = [50, 58, 54, 47, 90, 85, 120];
 const arrayMax2=  Math.max(...numbers2);
-console.log(arrayMax2)
+// console.log(arrayMax2)
+
+
+// use spread operators for copy
+
+const friends = ['shafi', 'pulak', 'ridwan', 'morshed'];
+const dosto = friends;
+const bondhu = [...friends] //copy
+console.log(dosto)
+dosto.push('hasan')
+console.log(bondhu)
+console.log(friends)
+
+const addExtra = [...friends, 'habu']
+console.log(addExtra)
